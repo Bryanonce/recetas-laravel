3a712392dfc4e1ab952cf128538cc94551929e75
Bryan <bryan.cedeo@gmail.com>
2021-01-14 18:50:04 -0500

    arreglando el bug del like

--- resources/views/recetas/show.blade.php
+++ resources/views/recetas/show.blade.php
@@ -27,90 +27,90 @@
             </div>
             
         </div>
-        <div class="mb-5 d-flex justify-content-center shadow p-3">
-            <div class="receta-meta mt-2">
-                <p>
-                    <span 
-                        class=
-                        "
-                            font-wigth-bold 
-                            text-primary
-                        "
-                    >
-                        Escrito en:
-                    </span>
-                    {{$receta->categoria->nombre}}
-                </p>
-                <p>
-                    <span 
-                        class=
-                        "
-                            font-wigth-bold 
-                            text-primary
-                        "
-                    >
-                        Autor:
-                    </span>
-                    {{-- TODO: mostrar el usuario --}}
-                    <a class="text-danger" href="{{route('perfiles.show',$receta->user->perfil->id)}}">{{$receta->user->name}}</a>                
-                </p>
-                <p>
-                    <span 
-                        class=
-                        "
-                            font-wigth-bold 
-                            text-primary
-                        "
-                    >
-                        Fecha de Creación:
-                    </span>
-                    <fecha-receta 
-                        fecha="{{$receta->categoria->created_at}}"
-                    ></fecha-receta>
-                    {{--$receta->categoria->created_at--}}
-                </p>
-                
-                <p>
-                    <span 
-                        class=
-                        "
-                            font-wigth-bold 
-                            text-primary
-                        "
-                    >
-                        Ultima Actualización:
-                    </span>
-                    <fecha-receta 
-                        fecha="{{$receta->categoria->updated_at}}"
-                    ></fecha-receta>
-                    {{--$receta->categoria->updated_at--}}
-                </p>
-    
-                <div class="ingredientes">
-                    <h2 
-                        class="my-2 text-primary"
-                    >
-                        Ingredientes:
-                    </h2>
-                    {!! $receta->ingredientes !!}
+        <div class="mb-5  shadow p-3">
+            <div class="receta-meta mt-2 d-flex justify-content-left">
+                <div>
+                    <p>
+                        <span 
+                            class=
+                            "
+                                font-wigth-bold 
+                                text-primary
+                            "
+                        >
+                            Escrito en:
+                        </span>
+                        {{$receta->categoria->nombre}}
+                    </p>
+                    <p>
+                        <span 
+                            class=
+                            "
+                                font-wigth-bold 
+                                text-primary
+                            "
+                        >
+                            Autor:
+                        </span>
+                        {{-- TODO: mostrar el usuario --}}
+                        <a class="text-danger" href="{{route('perfiles.show',$receta->user->perfil->id)}}">{{$receta->user->name}}</a>                
+                    </p>
+                    <p>
+                        <span 
+                            class=
+                            "
+                                font-wigth-bold 
+                                text-primary
+                            "
+                        >
+                            Fecha de Creación:
+                        </span>
+                        <fecha-receta 
+                            fecha="{{$receta->categoria->created_at}}"
+                        ></fecha-receta>
+                        {{--$receta->categoria->created_at--}}
+                    </p>
+                    
+                    <p>
+                        <span 
+                            class=
+                            "
+                                font-wigth-bold 
+                                text-primary
+                            "
+                        >
+                            Ultima Actualización:
+                        </span>
+                        <fecha-receta 
+                            fecha="{{$receta->categoria->updated_at}}"
+                        ></fecha-receta>
+                        {{--$receta->categoria->updated_at--}}
+                    </p>
+        
+                    <div class="ingredientes">
+                        <h2 
+                            class="my-2 text-primary"
+                        >
+                            Ingredientes:
+                        </h2>
+                        {!! $receta->ingredientes !!}
+                    </div>
+        
+                    <div class="preparacion">
+                        <h2 
+                            class="my-2 text-primary"
+                        >
+                            Preparación:
+                        </h2>
+                        {!! $receta->preparacion !!}
+                    </div>
                 </div>
-    
-                <div class="preparacion">
-                    <h2 
-                        class="my-2 text-primary"
-                    >
-                        Preparación:
-                    </h2>
-                    {!! $receta->preparacion !!}
-                </div>
-    
-                <like-button
+            </div>
+            <like-button
                     receta-id="{{$receta->id}}"
                     like = "{{$like}}"
                     likes = "{{$likes}}"
                 ></like-button>
-    
-            </div>
             
         </div>
         
